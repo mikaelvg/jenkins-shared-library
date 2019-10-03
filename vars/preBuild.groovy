2ebc24df8f234df7aed0f696f78a3b4787967d2f
Mikael <mikael@devkinetics.com>
2019-10-03 23:25:02 +0800

    For Squashing

--- vars/preBuild.groovy
+++ vars/preBuild.groovy
@@ -10,11 +10,6 @@ def call(body) {
             parameters([string(defaultValue: '', description: '', name: 'run_stages')])
     ])
 
-    stage('Prepare') {
-        abortAllPreviousBuildInProgress(currentBuild)
-        echo "BRANCH_NAME=${env.BRANCH_NAME}\nCHANGE_ID=${env.CHANGE_ID}\nCHANGE_TARGET=${env.CHANGE_TARGET}\nBUILD_URL=${env.BUILD_URL}"
-    }
-
     stage('Checkout') {
         checkout scm
 
