9814d8b5fdcf32bc1fcf3cd778b096828ecb7c29
Mikael <mikael@devkinetics.com>
2019-10-03 22:40:27 +0800

    Initial Commit

---
+++ vars/preBuild.groovy
@@ -0,0 +1,24 @@
+def call(body) {
+    def context= [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = context
+    body()
+
+    properties([
+            buildDiscarder(logRotator(artifactDaysToKeepStr: '', artifactNumToKeepStr: '', daysToKeepStr: '', numToKeepStr: '10')),
+            durabilityHint('MAX_SURVIVABILITY'),
+            parameters([string(defaultValue: '', description: '', name: 'run_stages')])
+    ])
+
+    stage('Prepare') {
+        abortAllPreviousBuildInProgress(currentBuild)
+        echo "BRANCH_NAME=${env.BRANCH_NAME}\nCHANGE_ID=${env.CHANGE_ID}\nCHANGE_TARGET=${env.CHANGE_TARGET}\nBUILD_URL=${env.BUILD_URL}"
+    }
+
+    stage('Checkout') {
+        node('build') {
+            checkout scm
+        }
+    }
+
+}
